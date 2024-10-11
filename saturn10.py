6b941831d5d522eaa83db380f280c2f2b3a30334
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2024-10-11 16:44:15 +0530

    Update saturn10.py

---
+++ saturn10.py
@@ -0,0 +1,5 @@
+Furthermore, when you log in using an LDAP user and local operators are disabled, several menu items, buttons and forms will be greyed out in order to avoid accounting operations on both local operators or LDAP users. It will be still possible to associate LDAP groups with BigFix roles and to login with an LDAP user belonging to an LDAP group associated with a BigFix role.
+
+The roles will be inherited according to the LDAP groups - BigFix roles association.
+
+Under Tools, the following menu items will be greyed out:
